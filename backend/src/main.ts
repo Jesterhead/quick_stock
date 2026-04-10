6929f0e6fdebd4138f1686867734f44f37ea9874
Jesterhead0302 <Jesterhead0302@gmail.com>
2026-04-10 15:08:22 -0400

    Reverting changes to backend app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,10 @@ if (!globalThis.crypto) {
 }
 
 import * as dotenv from 'dotenv';
-import * as express from 'express';
 import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthSeeding } from './auth/auth.seeding';
-import type { Response } from 'express';
 
 const envPath = path.resolve(process.cwd(), '.env.local');
 dotenv.config({ path: envPath });
@@ -26,21 +24,6 @@ async function bootstrap() {
       : 'http://localhost:4200',
     credentials: true,
   });
-
-  if (nodeEnv === 'production') {
-    const frontendPath = path.join(__dirname, '../../frontend/dist/frontend');
-    console.log('Serving frontend from:', frontendPath);
-    
-    app.use(express.static(frontendPath));
-    
-    app.use((req, res: Response, next) => {
-      if (!req.path.startsWith('/api') && !req.path.startsWith('/auth')) {
-        res.sendFile(path.join(frontendPath, 'index.html'));
-      } else {
-        next();
-      }
-    });
-  }
 
   const seeding = app.get(AuthSeeding);
   await seeding.seed();
